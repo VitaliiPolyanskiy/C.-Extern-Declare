46514d9f7faaf283ed3408a9eeef0137a1b745af
Vitaliy <vitaliypolyanskiy@gmail.com>
2024-12-06 00:04:24 +0200

    Add project files.

---
+++ Variable Scope/Main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Variable Scope.h"
+
+using namespace std;
+
+void main()
+{
+	int z = 8; // локальная переменная
+	cout << "Local variable: " << z << endl;
+	function();
+	function();
+	extern int a; // внешнее объявление глобальной переменной a
+	cout << "Global variable: " << a << endl; // обращение к глобальной переменной a
+	extern int b; // внешнее объявление статической глобальной переменной b
+	//cout << "Global static variable: " << b << endl; // ошибка на этапе компоновки
+}
---
+++ Variable Scope/Variable Scope.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int a = 5; // определение глобальной переменной
+static int b = 5; // определение статической глобальной переменной
+
+void function()
+{
+	a += 2; // обращение к глобальной переменной a
+	b += 2; // обращение к статической глобальной переменной b
+	cout << "Global variable: " << a << endl;
+	cout << "Global static variable: " << b << endl;
+}
+
